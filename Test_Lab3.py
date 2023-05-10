dd30d042fba5e909428a7f7e0fabcebaa6a90398
Jun Kei <teo.junkei@hotmail.com>
2023-05-10 12:26:42 +0800

    *

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,29 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+def test_bubble_sort_more_than_ten():
+    result = []
+    input_arr = [23, 33, 45, 65, 54, 78, 12, 24, 43, 95, 67]
+    result = len(input_arr)
+
+    if result > 10 :
+        x = 1
+        print(x)
+    assert(x==1)
+    return x
+def test_bubble_sort_zero():
+    result = []
+    input_arr = []
+    result = len(input_arr)
+
+    assert(result ==0)
+    return result
+def test_bubble_sort_not_integers():
+    result = []
+    input_arr = [10.5, 6, 7, 8, 15, 16]
+    result=(type(input_arr))
+    if result != int:
+        z = 2
+        print(z)
+    assert(z==2)
+    return z
